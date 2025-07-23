c9a0eeaecf9d6d793ba5ef606258e5b29e4c2672
ana-bellegit <anabelle12anabelle@gmail.com>
2025-07-23 09:20:00 -0300

    Css

---
+++ View/login.php
@@ -0,0 +1,30 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <link rel="stylesheet" href="../Templates/Assets/CSS/loginCadastro.css"
+</head>
+<body>
+    <main>
+             <img src="" alt="">
+             <h2> Crie sua conta </h2>
+             <p>Bem-vindo! Preencha os detalhes para começar</p>
+             
+
+             <form action="">
+             <label for="NomeDeUsuário"> <input type="text" name="Nome de usuário"></label>
+             <label for="Email"> <input type="email" name=" Endereço de email"></label>
+             <label for="Senha"><input type="text" name="Senha"></label>
+             <input type="submit" value="continuar"> 
+             </form>
+
+             <div class= "footer"> <p> Já tem uma conta? </p> <a href=""> Faça login </a></div>
+        </main>
+</body>
+</html>
+
