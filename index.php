0afb2f153f682eb381889bf737d06a604db199f3
amandadua <amandaduartesantos13@outlook.com>
2025-07-24 06:21:40 -0300

    Feature/Página Inicial estilo e estrutura

--- index.php
+++ index.php
@@ -1,11 +1,37 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ToDo</title>
+    <link rel="stylesheet" href="Templates/Assets/CSS/global.css">
+    <link rel="stylesheet" href="Templates/Assets/CSS/index.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 <body>
-    aaa
-</body>
-</html>
+    <header class="header">
+        <div class="container">
+            <div class="nav">
+                <div class="logo">
+                    <span class="logo-texto">ToDo</span>
+                </div>
+                <div class="nav-botoes">
+                    <a href="" class="btn btn-secondary">Login</a>
+                    <a href="" class="btn btn-primary">Cadastro</a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="nome">
+        <div class="container">
+            <div class="conteudo-nome">
+                <h1 class="titulo-nome">
+                    <span class="titulo-linha">ToDo Simplifica</span>
+                    <span class="titulo-linha-estilo">Gerenciamento de projetos</span>
+                </h1>
+                <p class="titulo-legenda">Uma ferramenta gratuita de gerenciamento de projetos criada para pequenos projetos.</p>
+                <a href="" class="btn botao-cadastro">Começar</a>
+            </div>
+        </div>
+    </section>
